dd1f779d02013ad46508c54acc0a1ebd898a4b6a
Patrick Abrena <patrickabrena@hotmail.com>
2024-01-18 20:25:01 -0600

    leaving myself a message for tomrrow

---
+++ added-typing-effect.js
@@ -0,0 +1,4 @@
+/*bringing in js from slot-effect-on-load and adding in typing effect feature AFTER 
+    the for loops are done iterating
+    
+    -will probably have to use promises to achieve effect*/
