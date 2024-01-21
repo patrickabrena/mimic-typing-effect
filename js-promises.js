580023c5d30d57d2ddf66b7016b66fef80d36b3e
Patrick Abrena <patrickabrena@hotmail.com>
2024-01-21 16:05:28 -0600

    create a delayed console.log .then() from promise I included in repeatForLoop()
    lines 120-128 in added-typing-effect.js is the promise
    lines 184-192 is the .then and .catch for the promise
    also added js-promises.js file that has the basic solution for promises

---
+++ js-promises.js
@@ -0,0 +1,35 @@
+function myFunction() {
+  return new Promise((resolve, reject) => {
+    //synchronus or asynchronous logic here
+    //For example, you can use setTimieout to simulate an asynchrounous operations
+    setTimeout(() => {
+      const success = true; //assume the operation was successful
+
+      if (success) {
+        console.log("Function completed!");
+        resolve(); // Resolve the promise to indicate that the function is done
+      } else {
+        const error = new Error("Something went wrong");
+        console.error(error);
+        reject(error); // Reject the promise with an error
+      }
+    }, 1000);
+  });
+}
+
+// Usage
+myFunction()
+  .then(() => {
+    console.log("Code to be executed after myFunction is done. ");
+  })
+  .catch((error) => {
+    console.error("An error occurred:", error);
+  });
+
+/*
+In this modified example, if the "success" variable is "true", the
+promise is resolved, and the code inside the ".then()" block will be 
+executed. If "success" is "false", an error is created, and te promise is rejected,
+triggering the code inside the ".catch()" block. This way, you can handle both 
+successful and error scenarious within the "myFunction"
+ */
